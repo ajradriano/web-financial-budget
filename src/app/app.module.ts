77c76c04d8e232c643f3efb314f37020676fc89c
Adriano J Rodrigues <adrianojrodrigues12@gmail.com>
2024-01-09 23:10:37 -0300

    update angular version to 17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Libs
+import { ButtonModule } from 'primeng/button';
+
+// Components
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routing.modules';
@@ -8,11 +12,12 @@ import { AppRoutingModule } from './app-routing.modules';
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
