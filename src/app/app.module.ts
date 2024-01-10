99ffddadc8166cb40bbcd4cfe1fbaa0ff8984514
Adriano J Rodrigues <adrianojrodrigues12@gmail.com>
2024-01-09 22:29:19 -0300

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.modules';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
