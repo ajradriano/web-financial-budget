1064485e0baa548c1d11a3b25c41edc02332902a
Adriano J Rodrigues <adrianojrodrigues12@gmail.com>
2024-01-09 23:59:39 -0300

    Define SCSS default + ajuste TSLIB + Implementação sidebar + organização arquivos projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Libs
 import { ButtonModule } from 'primeng/button';
 
 // Components
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PrimeNgModules } from './prime-ng.modules';
 import { AppRoutingModule } from './app-routing.modules';
+import { SidebarComponent } from './template/menu/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
+    SidebarComponent 
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    ButtonModule
+    PrimeNgModules
+  ],
+  exports: [
+    PrimeNgModules
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/prime-ng.modules.ts
+++ src/app/prime-ng.modules.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { SidebarModule } from 'primeng/sidebar';
+import { PanelMenuModule } from 'primeng/panelmenu';
 
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    MenubarModule,
+    SidebarModule,
+    PanelMenuModule,
+    MenuModule,
+  ],
+  exports: [
+    ButtonModule,
+    MenubarModule,
+    SidebarModule,
+    PanelMenuModule,
+    MenuModule,
+  ],
 })
 export class PrimeNgModules {}
