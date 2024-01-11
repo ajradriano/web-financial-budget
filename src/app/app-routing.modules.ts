58d3c0ac68cf0598f58cf7ae823b704acd8ec59e
Adriano J Rodrigues <adrianojrodrigues12@gmail.com>
2024-01-10 23:05:31 -0300

    add routes

--- src/app/app-routing.modules.ts
+++ src/app/app-routing.modules.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-// import { AboutComponent } from './about.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { TypesComponent } from './components/types/types.component';
+import { PaymentMethodComponent } from './components/payment-method/payment-method.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
-//   { path: 'about', component: AboutComponent },
+  { path: 'categorias', component: CategoriesComponent },
+  { path: 'tipos-movimento', component: TypesComponent },
+  { path: 'metodos-pagamento', component: PaymentMethodComponent },
 ];
 
 @NgModule({
