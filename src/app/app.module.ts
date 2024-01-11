e19175c6117611eb8fa7ebaa7cccd91601cbaafd
Adriano J Rodrigues <adrianojrodrigues12@gmail.com>
2024-01-10 23:06:22 -0300

    Add imports to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Libs
-import { ButtonModule } from 'primeng/button';
+import { PrimeNgModule } from './prime-ng.module';
 
 // Components
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { PrimeNgModules } from './prime-ng.modules';
 import { AppRoutingModule } from './app-routing.modules';
 import { SidebarComponent } from './template/menu/sidebar/sidebar.component';
+import { FooterComponent } from './template/layout/footer/footer.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { TitleComponent } from './template/layout/title/title.component';
+import { ListComponent } from './template/layout/list/list.component';
+import { TypesComponent } from './components/types/types.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    SidebarComponent 
+    SidebarComponent,
   ],
   imports: [
+    PrimeNgModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    PrimeNgModules
+    FooterComponent,
+    TitleComponent,
+    ListComponent,
+    CategoriesComponent,
+    TypesComponent
   ],
-  exports: [
-    PrimeNgModules
-
-  ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
