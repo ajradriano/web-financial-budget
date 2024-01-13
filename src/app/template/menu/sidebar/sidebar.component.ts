f86c8aa29801c11e0ca47ac5d65b4761c96509d4
Adriano J Rodrigues <adrianojrodrigues12@gmail.com>
2024-01-13 01:58:42 -0300

    Ajustes sidebar

--- src/app/template/menu/sidebar/sidebar.component.ts
+++ src/app/template/menu/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
     selector: 'app-sidebar',
@@ -8,10 +9,54 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
 
     showSideBar: boolean = false;
-
+    menu: MenuItem[] | undefined;
+    
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.menu = [
+            {
+                label: 'Home',
+                icon: 'pi pi-fw pi-home',
+                routerLink: [''],
+                command: () => {
+                    this.openMenu()
+                }
+            },
+            {
+                label: 'Movimentação Financeira',
+                icon: 'pi pi-fw pi-wallet',
+                routerLink: ['/movimentacao-financeira'],
+                command: () => {
+                    this.openMenu()
+                }
+            },
+            {
+                label: 'Categorias',
+                icon: 'pi pi-fw pi-list',
+                routerLink: ['/categorias'],
+                command: () => {
+                    this.openMenu()
+                }
+            },
+            {
+                label: 'Tipos de Movimentação',
+                icon: 'pi pi-fw pi-file',
+                routerLink: ['/tipos-movimento'],
+                command: () => {
+                    this.openMenu()
+                }
+            },
+            {
+                label: 'Meios de Pagamento',
+                icon: 'pi pi-fw pi-money-bill',
+                routerLink: ['/metodos-pagamento'],
+                command: () => {
+                    this.openMenu()
+                }
+            },
+        ];
+    }
 
     openMenu() {
         this.showSideBar = !this.showSideBar;
